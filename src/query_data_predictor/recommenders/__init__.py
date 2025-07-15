fe51c16f773879f689e9e961b8a42a72c66f5076
David Adams <david@dadams2.com>
2025-07-15 13:41:30 +1000

    add extra reccomenders to init

--- src/query_data_predictor/recommenders/__init__.py
+++ src/query_data_predictor/recommenders/__init__.py
@@ -7,11 +7,27 @@ from .dummy_recommender import DummyRecommender
 from .clustering_recommender import ClusteringRecommender
 from .random_recommender import RandomRecommender
 from .interestingness_recommender import InterestingnessRecommender
+from .similarity_recommender import SimilarityRecommender
+from .frequency_recommender import FrequencyRecommender
+from .sampling_recommender import SamplingRecommender
+from .hierarchical_recommender import HierarchicalRecommender
+from .index_recommender import IndexRecommender
+from .incremental_recommender import IncrementalRecommender
+from .embedding_recommender import EmbeddingRecommender
+from .query_expansion_recommender import QueryExpansionRecommender
 
 __all__ = [
     'BaseRecommender',
     'DummyRecommender', 
     'ClusteringRecommender',
     'RandomRecommender',
-    'InterestingnessRecommender'
+    'InterestingnessRecommender',
+    'SimilarityRecommender',
+    'FrequencyRecommender',
+    'SamplingRecommender',
+    'HierarchicalRecommender',
+    'IndexRecommender',
+    'IncrementalRecommender',
+    'EmbeddingRecommender',
+    'QueryExpansionRecommender'
 ]
